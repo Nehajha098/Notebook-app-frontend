1b0c9a74380e12e37e56a0c4df28cf346037d573
Neha Jha <nehajha22004@gmail.com>
2026-01-12 12:04:31 +0530

    updated signin and navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,13 @@
 import "./navbar.css"
+import logo from "../assets/logo notebook.png";
 
 function Navbar() {
     return(
         <div className="nav">
             <h3>Notely</h3>
-            <div className="logo"></div>
+            <div className="logo">
+                <img src={logo} alt="notebook logo"></img>
+            </div>
         </div>
     );
 }
